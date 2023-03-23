06abe15a619c884899e5ca39ab30afd1a2f8d355
josoya <thdi4564@nate.com>
2023-03-23 14:54:07 +0900

    querydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -17,6 +18,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -96,5 +98,21 @@ class MemberRepositoryTest {
 
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+    }
+
+    /**
+     * join x
+     * 복잡한 실무환경에서 사용하기에는 한계가 명확하다.
+     * 사용 x
+     */
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(0, 40)
+                        .and(member.username.eq("member1")));
+
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
     }
 }
